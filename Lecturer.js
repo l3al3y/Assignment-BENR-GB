373d4be858f265a69d57585e6bbf68ab79a74743
Farisem <b022210095@student.utem.edu.my>
2024-01-13 00:57:15 +0800

    test1

--- Lecturer.js
+++ Lecturer.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const bcryptjs = require('bcrypt')
+app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
