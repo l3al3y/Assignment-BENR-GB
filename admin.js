d71d0f4f004753d687304131300c0843e1684992
Syamim <b022210085@student.utem.edu.my>
2024-01-12 23:23:55 +0800

    Student

--- admin.js
+++ admin.js
@@ -9,24 +9,24 @@ const uri = "mongodb+srv://groupb:abc12345@groupb.6djtmth.mongodb.net/?retryWrit
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -35,19 +35,19 @@ app.post('/admin', (req, res) => {
     const admin = req.body;
     students.push(student);
     res.send(`Admin ${admin.name} added`);
-  });
+});
 
-  app.post('/register', (req, res) => {
-    const {username,password}=req.body
-    console.log(username,password);
-  
-    const hash= bcryptjs.hashSync(password,10);
-    client.db("groupb.groupb").collection("users").
-    insertOne({"username": req.body.username,"password": hash });
+app.post('/Admin/AddStudent', (req, res) => {
+    const { username, password } = req.body
+    console.log(username, password);
+
+    const hash = bcryptjs.hashSync(password, 10);
+    client.db("ManagementSystem").collection("users").
+        insertOne({ "username": req.body.username, "password": hash });
     res.send('Register successfully');
     console.log(hash);
-  })
+})
 
-  app.post('/View Detail', (req, res) => {
-  })
-    
+app.post('/View Detail', (req, res) => {
+
+})
