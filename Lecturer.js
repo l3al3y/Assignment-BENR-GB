71bc98a7dff14366c614e2b706883925ca3e6e12
Farisem <b022210095@student.utem.edu.my>
2024-01-12 23:35:18 +0800

    MANGG0

--- Lecturer.js
+++ Lecturer.js
@@ -1,5 +1,54 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const bcryptjs = require('bcryptjs')
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://groupb:abc12345@groupb.6djtmth.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.post('/lecturer', (req, res) => {
     const lecturer = req.body;
   lecturer.push(lecturer);
   res.send(`Lecturer ${lecturer.name} added`);
 });
+
+app.post('/lecturer/login', (req, res) => {
+    
+})
+
+app.post('/lecturer/View Detail', (req, res) => {
+})
+
+app.post('/lecturer/Student list', (req, res) => {
+})
+
+app.post('/lecturer/View report', (req, res) => {
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  }); 
